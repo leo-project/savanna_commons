96e3e46199c284eb9da73fb57acad5b15864013c
Yosuke Hara <yousuke.hara@gmail.com>
2014-07-11 15:41:19 +0900

    Refactor metric result related

--- src/svc_metrics_histogram.erl
+++ src/svc_metrics_histogram.erl
@@ -77,11 +77,12 @@ trim_and_notify(#sv_metric_state{id = ServerId,
     %% then clear oldest data
     case svc_tbl_metric_group:get(MetricGroup) of
         {ok, #sv_metric_group{schema_name = SchemaName}} ->
-            catch Callback:notify(Result#sv_result{schema_name = SchemaName,
+            catch Callback:notify(Result#sv_result{metric_type = ?METRIC_HISTOGRAM,
+                                                   schema_name = SchemaName,
                                                    metric_group_name = MetricGroup,
                                                    col_name = Key,
-                                                   samples = Samples,
-                                                   result = Stats}),
+                                                   result = {Samples,Stats}
+                                                  }),
             try
                 ok = trim_1(SampleType, ServerId)
             catch
